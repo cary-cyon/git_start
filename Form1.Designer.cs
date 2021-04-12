5c034a013be5a804066c1eced344c9e1b025b0d8
cary-cyon <kozvlad99@gmail.com>
2021-04-12 20:37:11 +0300

    our work

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace test_w_cam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.старт = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.старт,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
+            this.toolStripComboBox1.Click += new System.EventHandler(this.toolStripComboBox1_Click);
+            // 
+            // старт
+            // 
+            this.старт.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.старт.Image = ((System.Drawing.Image)(resources.GetObject("старт.Image")));
+            this.старт.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.старт.Name = "старт";
+            this.старт.Size = new System.Drawing.Size(42, 22);
+            this.старт.Text = "Старт";
+            this.старт.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(38, 22);
+            this.toolStripButton2.Text = "Стоп";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripButton старт;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using DirectShowLib;
+using ZXing;
+namespace test_w_cam
+{
+    public partial class Form1 : Form
+    {
+        private System.ComponentModel.IContainer components = null;
+        private VideoCapture capture = null;
+        private DsDevice[] web_cam = null;
+        private int select = 0;
+        private IBarcodeReader reader = new BarcodeReader();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            web_cam = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+
+            for(int i =0; i< web_cam.Length; i++)
+            {
+                toolStripComboBox1.Items.Add(web_cam[i].Name);
+            }
+            try
+            {
+                if(web_cam.Length == 0)
+                {
+                    throw new Exception("У вас нет камер");
+                }
+                else if(web_cam == null)
+                {
+                    Console.WriteLine("lol");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("het");
+            }
+        }
+
+        private void toolStripComboBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(web_cam.Length == 0)
+                {
+                    throw new Exception("НЕТ КАМЕР");
+                }
+                else if (toolStripComboBox1.SelectedItem ==null)
+                {
+                    throw new Exception("НЕТ ВЫБОРА");
+                }
+                else if(capture != null)
+                {
+                    capture.Start();
+                }
+                else
+                {
+                    capture = new VideoCapture(select);
+                    capture.ImageGrabbed += Capture_ImageGrabbed;
+                    capture.Start();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private void Capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Mat new_mat = new Mat();
+                capture.Retrieve(new_mat);
+                var barcodeBitmap = new_mat.ToImage<Bgr, byte>().Flip(Emgu.CV.CvEnum.FlipType.Horizontal).ToBitmap();
+                var result = reader.Decode(barcodeBitmap);
+                pictureBox1.Image = barcodeBitmap;
+                // do something with the result
+                if (result != null)
+                {
+                    Console.WriteLine(result.Text);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("ОШИБКА");
+            }
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            select = toolStripComboBox1.SelectedIndex;
+        }
+    }
+}
